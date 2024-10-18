ac896a9693c6df3895fd12d1f6e0bf8a3adc6dd1
Anas Farooq <anasfarooq2006@gmail.com>
2024-10-19 01:48:19 +0500

    Added testimonials swiper

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -57,49 +57,49 @@ export const testimonialsData = [
         text: "I've been using bg.removal for nearly 6 months, I had a fantastic experience. The quality is top-notch. I recommend others to try this app.",
         author: "Donald Jackman",
         image: profile_img_2,
-        jobTitle:'UI Deginer'
+        jobTitle:'UI Designer'
     },
     {
         id: 3,
         text: "I've been using bg.removal for nearly 6 months, I had a fantastic experience. The quality is top-notch. I recommend others to try this app.",
         author: "Donald Jackman",
         image: profile_img_2,
-        jobTitle:'UI Deginer'
+        jobTitle:'UX Expert'
     },
     {
         id: 4,
         text: "I've been using bg.removal for nearly 6 months, I had a fantastic experience. The quality is top-notch. I recommend others to try this app.",
         author: "Donald Jackman",
         image: profile_img_2,
-        jobTitle:'UI Deginer'
+        jobTitle:'Backend developer'
     },
     {
         id: 5,
         text: "I've been using bg.removal for nearly 6 months, I had a fantastic experience. The quality is top-notch. I recommend others to try this app.",
         author: "Donald Jackman",
         image: profile_img_2,
-        jobTitle:'UI Deginer'
+        jobTitle:'Wordpress Developer'
     },
     {
         id: 6,
         text: "I've been using bg.removal for nearly 6 months, I had a fantastic experience. The quality is top-notch. I recommend others to try this app.",
-        author: "Donald Jackman",
+        author: "Graphic Designer",
         image: profile_img_2,
-        jobTitle:'UI Deginer'
+        jobTitle:'Product Manager'
     },
     {
         id: 7,
         text: "I've been using bg.removal for nearly 6 months, I had a fantastic experience. The quality is top-notch. I recommend others to try this app.",
         author: "Donald Jackman",
         image: profile_img_2,
-        jobTitle:'UI Deginer'
+        jobTitle:'Content Writer'
     },
     {
         id: 8,
         text: "I've been using bg.removal for nearly 6 months, I had a fantastic experience. The quality is top-notch. I recommend others to try this app.",
         author: "Donald Jackman",
         image: profile_img_2,
-        jobTitle:'UI Deginer'
+        jobTitle:'Logo Designer'
     },
 ];
 
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,36 +1,67 @@
-import React from 'react'
-import { testimonialsData } from '../assets/assets'
+import React, { useEffect, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules'; 
+import 'swiper/swiper-bundle.css';
+import { testimonialsData } from '../assets/assets'; 
 
 const Testimonials = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <div>
+    <div className='mx-5 sm:mx-28 '>
+      {/* Title */}
+      <h1 className='mb-20 text-center text-2xl md:text-3xl lg:text-4xl py-5 font-semibold bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text text-transparent'>
+        <span className='bg-gradient-to-r from-violet-600 to-fuchsia-500 bg-clip-text text-transparent'>Customer</span> Testimonials
+      </h1>
 
-{/* Title */}
+      {/* Swiper Container */}
 
-<h1 className='text-center text-2xl md:text-3xl lg:text-4xl py-5 font-semibold bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text text-transparent'><span className='bg-gradient-to-r from-violet-600 to-fuchsia-500 bg-clip-text text-transparent'>Customers</span> Testimonials
-</h1>
-
-<div className='grid grid-cols-1 md:grid-cols-2 gap-10 max-w-4xl mx-auto px-4 py-8'>
-
-{testimonialsData.map((item,index)=>(
-  <div className='bg-white rounded-xl p-6 drop-shadow-md max-w-lg m-auto hover:scale-105 transition-all duration-700' key={index}>
-<p className='text-4xl text-gray-500'>"</p>
-<p className='text-sm text-gray-500'>{item.text}</p>
-<div className='flex items-center gap-3 mt-5'>
-  <img className='w-9 rounded-full' src={item.image} alt="" />
-  <div>
-    <p>{item.author}</p>
-    <p>{item.jobTitle}</p>
-  </div>
-</div>
-  </div>
-
-))}
-
-</div>
-
+      <div className="mx-4 md:mx-8">
+        <Swiper
+          modules={[Autoplay]} 
+          autoplay={{ delay: 3000 }}
+          loop={true} 
+          spaceBetween={30} 
+          breakpoints={{
+            240: {
+              slidesPerView: 1,
+            },
+            768: {
+              slidesPerView: 2,
+            },
+            1204: {
+              slidesPerView: 4,
+            },
+          }}
+        >
+          {testimonialsData.map((item) => (
+            <SwiperSlide key={item.id} className='bg-white rounded-xl p-6 drop-shadow-md hover:scale-105 transition-all cursor-default'>
+              <p className='bg-gradient-to-r from-violet-600 to-fuchsia-500 bg-clip-text text-transparent text-4xl'>"</p>
+              <p className='text-sm text-gray-500'>{item.text}</p>
+              <div className='flex items-center gap-3 mt-5'>
+                <img className='w-9 rounded-full' src={item.image} alt={item.author} />
+                <div>
+                  <p className='bg-gradient-to-r from-gray-900 to-gray-700 bg-clip-text text-transparent'>{item.author}</p>
+                  <p  className='bg-gradient-to-r from-violet-600 to-fuchsia-500 bg-clip-text text-transparent '>{item.jobTitle}</p>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Testimonials
+export default Testimonials;
