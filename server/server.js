9e9114a162823c1425aac3ac17bb3afb75d6ad81
Anas Farooq <anasfarooq2006@gmail.com>
2024-10-20 19:57:29 +0500

    added some changes

--- server/server.js
+++ server/server.js
@@ -8,13 +8,18 @@ import connectDB from "./configs/mongodb.js"
 
 const PORT = process.env.PORT || 4000
 const app = express()
-const cors = require("cors")
+const corsConfig = {
+    origin: "*",
+    credential: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+};
 await connectDB()
 
 // Initialize Middleware 
 
 app.use(express.json())
-app.use(cors())
+app.options("", cors(corsConfig));
+app.use(cors(corsConfig))
 
 // API routes 
 
